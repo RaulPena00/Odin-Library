76d8972fa2dddf67d6e2d8e9290923cd19716275
Raul Pena Pereira <43591166+RaulPena00@users.noreply.github.com>
2022-09-29 13:29:29 -0300

    Added a "Read" button

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ newBookButton.addEventListener('click', function addBookToLibrary() {
     let title = prompt("Write the title of the book");
     let author = prompt("Write the author's name");
     let pages = prompt("Write the number of pages");
-    let read = prompt("Have you read it?");
+    let read = 'No'
 
     tableGenerator(title, author, pages, read)
 
@@ -39,34 +39,50 @@ function tableGenerator(title, author, pages, read) {
     //Create a delete button and call the event on click
     const deleteButton = document.createElement('button');
     deleteButton.innerHTML = 'Delete';
-    deleteButton.setAttribute('id', 'delBtn')
+    deleteButton.setAttribute('id', 'delBtn');
 
     deleteButton.addEventListener('click', () => { deleteBtn(deleteCell) })
 
+    //Create a 'Read' button that change the read table value
+    const readButton = document.createElement('button');
+    readButton.innerHTML = 'Read';
+    readButton.setAttribute('id', 'readBtn');
+
+    readButton.addEventListener('click', () => {readBtn(readCell)})
+
     // Insert new elements on the display table
     var deleteCell = row.insertCell();
-    var indexCell = row.insertCell();
     var cell1 = row.insertCell();
     var cell2 = row.insertCell();
     var cell3 = row.insertCell();
-    var cell4 = row.insertCell();
+    var readCell = row.insertCell();
+    var readBtnCell = row.insertCell();
 
-    var newIndex = document.createTextNode(indexCounter)
     var newTitle = document.createTextNode(title);
     var newAuthor = document.createTextNode(author);
     var newPages = document.createTextNode(pages);
     var newRead = document.createTextNode(read);
-
+    
     deleteCell.appendChild(deleteButton)
-    indexCell.appendChild(newIndex);
     cell1.appendChild(newTitle);
     cell2.appendChild(newAuthor);
     cell3.appendChild(newPages);
-    cell4.appendChild(newRead);
+    readCell.appendChild(newRead);
+    readBtnCell.appendChild(readButton)
 }
 
 //Delete function, target the parent Node of the inserted element
 function deleteBtn(row) {
     var parent = row.parentNode;
     parent.parentNode.removeChild(parent);
+}
+
+//Change the 'Read' value
+function readBtn(cell){
+    if (cell.innerHTML === 'No'){
+        cell.innerHTML = 'Yes';
+    }
+    else if (cell.innerHTML === 'Yes'){
+        cell.innerHTML = 'No';
+    }
 }
